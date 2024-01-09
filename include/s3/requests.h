394893f510f4fbeac4c8dadb1fb76d7168d0d820
Ilya Kobets <i.kobets@orioledb.com>
2024-01-09 01:43:57 +0100

    Error on startup with wrong credentials

--- include/s3/requests.h
+++ include/s3/requests.h
@@ -17,5 +17,6 @@ extern bool s3_put_file(char *objectname, char *filename);
 extern void s3_put_empty_dir(char *objectname);
 extern bool s3_put_file_part(char *objectname, char *filename, int partnum);
 extern void s3_get_file_part(char *objectname, char *filename, int partnum);
+extern void s3_list_objects(void);
 
 #endif							/* __S3_REQUESTS_H__ */
